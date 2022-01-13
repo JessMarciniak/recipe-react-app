ef41615393cff43b0213186ec3cf37c33c90eadd
JessMarciniak <92031714+JessMarciniak@users.noreply.github.com>
2022-01-13 14:04:54 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>What's for dinner?</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          What do you have on hand?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
